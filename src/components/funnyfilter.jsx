66e2a405b85e24860edc6c4c646fd21e86327582
rasthpop <rasthetrollman@example.com>
2025-01-26 00:46:31 +0200

    hobo

--- src/components/funnyfilter.jsx
+++ src/components/funnyfilter.jsx
@@ -17,15 +17,20 @@ export default function FunnyFilter(){
     }
 
     return(
-        <div className="relative">
-            <div onClick={() => (setOpen(!open))} className={`object cursor-pointer text-md xl:text-xl flex items-center bg-[#251607] text-[#FFF0E1] p-2 xl:p-3 px-4 xl:px-5 ${open ? "rounded-t-lg" : "rounded-lg"}`}>
-                <span>sorting by:</span><span className="ml-2 text-sm xl:text-lg leading-tight">{curSort}</span>
+        <div>
+            <div className=" rounded-lg object cursor-pointer text-md xl:text-xl flex items-center bg-[#251607] text-[#FFF0E1] p-2 xl:p-3 px-4 xl:px-5">
+                honoraries
             </div>
-            {open ? <div className="absolute z-[200] bg-[#251607] text-[#FFF0E1] w-full flex flex-col gap-2 pb-4 rounded-b-lg items-center">
-            {sort.map(category => (
-                <div onClick={changeSort} id={category} key={category} className="text-lg object">{category}</div>
-            ))}
-            </div> : null}
+            <div className="relative">
+                <div onClick={() => (setOpen(!open))} className={`object cursor-pointer text-md xl:text-xl flex items-center bg-[#251607] text-[#FFF0E1] p-2 xl:p-3 px-4 xl:px-5 ${open ? "rounded-t-lg" : "rounded-lg"}`}>
+                    <span>sorting by:</span><span className="ml-2 text-sm xl:text-lg leading-tight">{curSort}</span>
+                </div>
+                {open ? <div className="absolute z-[200] bg-[#251607] text-[#FFF0E1] w-full flex flex-col gap-2 pb-4 rounded-b-lg items-center">
+                {sort.map(category => (
+                    <div onClick={changeSort} id={category} key={category} className="text-lg object">{category}</div>
+                ))}
+                </div> : null}
+            </div>
         </div>
     )
 }
